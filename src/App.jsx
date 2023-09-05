b4b41a69da6262c61a24a76af23f95b723a822ae
f-rahim <freelancerrahim26@gmail.com>
2023-09-05 10:51:49 +0600

    Added initial Main layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import { RouterProvider } from "react-router-dom";
 import "./App.css";
+import router from "./router/router";
 
 function App() {
-  return <h1>Book Resell</h1>;
+  return <RouterProvider router={router}></RouterProvider>;
 }
 
 export default App;
