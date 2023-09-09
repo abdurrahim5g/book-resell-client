d61b448a2a0febd63917b5d482c7ae3094339133
f-rahim <freelancerrahim26@gmail.com>
2023-09-09 12:27:38 +0600

    Added Dashboard Page

---
+++ src/dashboard/Dashboard/Dashboard.jsx
@@ -0,0 +1,11 @@
+import AdminDashboard from "../AdminDashboard/AdminDashboard";
+
+const Dashboard = () => {
+  return (
+    <>
+      <AdminDashboard />
+    </>
+  );
+};
+
+export default Dashboard;
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -2,13 +2,13 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../layouts/Main";
 import Home from "../pages/Home/Home";
 import DBLayout from "../layouts/DBLayout";
-import AdminDashboard from "../dashboard/AdminDashboard/AdminDashboard";
 import DashboardContex from "../contex/DashboardContex";
 import Sellers from "../dashboard/AdminDashboard/Sellers/Sellers";
 import Buyers from "../dashboard/AdminDashboard/Buyers/Buyers";
 import Login from "../pages/Login/Login";
 import Signup from "../pages/Signup/Signup";
 import DashboardRoute from "./DashboardRoute";
+import Dashboard from "../dashboard/Dashboard/Dashboard";
 
 const router = createBrowserRouter([
   {
@@ -30,7 +30,7 @@ const router = createBrowserRouter([
     children: [
       {
         path: "/dashboard",
-        element: <AdminDashboard />,
+        element: <Dashboard />,
       },
       {
         path: "sellers",
